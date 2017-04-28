f2f75cbab47395da0a569d029827569ac49805b8
Brenna Berlin <brenna@id.iit.edu>
2017-04-28 07:22:07 -0500

    jq parts 1 and 2

--- part-1/script.js
+++ part-1/script.js
@@ -1,28 +1,57 @@
-// Do your work in this file.
-var sandwich = $(".sandwich");
+// // Do your work in this file.
 
-console.log(sandwich);
+// 1. [Fork this repo][fork]. X
+// 1. Only code inside the file named `script.js`. No editing any other files. X
+// 1. Create a `div` element with a class name of "**view**" and append it to the document `body` using jQuery. You can use [append], [appendTo], or any other jQuery method.
+var skyElement = document.createElement("div");
+skyElement.className = "view";
+// document.body.appendChild(skyElement);
+$('body').append(skyElement);
 
-var ourSandwich = $("#sandwich-cheese");
+// 1. Create a `div` element with a class name of "**grass**" and append it to the previously created "view" div from step 3.
+var grassElement = document.createElement("div");
+grassElement.className = "grass";
+// skyElement.appendChild(grassElement);
+$('.view').append(grassElement);
 
-// console.log(ourSandwich.width());
-// console.log(ourSandwich.height());
-// console.log(sandwich.length);
-// console.log(sandwich.togleClass("cheese"));
+// 1. Create a `div` element with a class name of "**sun**" and append it to the previously created "view" div from step 3.
+var sunElement = document.createElement("div");
+sunElement.className = "sun";
+// skyElement.appendChild(sunElement);
+$('.view').append(sunElement);
 
-// ourSandwich.addClass('food delicious');
-// ourSandwich.removeClass('delicious');
-
-var lettuce = $('.lettuce');
-
-function lettuceClicked(){
-    var newHeight = $(this).height();
-    // console.log(height);
-   newHeight = newHeight + 20;
-
-    $(this).height(newHeight);
+// 1. Write a [`while`] loop to create 5 divs. The div classes will be "**ray-1**", "**ray-2**", "**ray-3**", "**ray-4**", and "**ray-5**". Append each of these divs to the previously created "sun" div from step 5.
+var i = 1;
+while (i <= 5) {
+    var rayElement = document.createElement("div");
+    rayElement.className = "ray-" + i;
+   $('.sun').append(rayElement);
+    i++;
 }
 
-lettuce.click(lettuceClicked);
 
+// 1. Create a `div` element with a class name of "**mountain**" and append it to the previously created "view" div from step 3.
+var mountainElement = document.createElement("div");
+mountainElement.className = "mountain";
+// skyElement.appendChild (mountainElement);
+$('.view').append(mountainElement);
+
+
+
+// 1. Create a `div` element with a class name of "**mountain-top**" and append it to the previously created "mountain" div from step 7.
+var mountainTopElement = document.createElement("div");
+mountainTopElement.className = "mountain-top";
+// mountainElement.appendChild (mountainTopElement);
+$('.mountain').append(mountainTopElement);
+
+// 1. Write a [`do...while`] loop to create 3 divs. The div classes will be "**mountain-cap-1**", "**mountain-cap-2**", and "**mountain-cap-3**". Append each of these divs to the previously created "mountain-top" div from step 8.
+var i = 1;
+do {
+    var capElement = document.createElement("div");
+    capElement.className = "mountain-cap-" + i;
+    // mountainTopElement.appendChild(capElement);
+    $('.mountain-top').append(capElement);
+    i++;
+}
+while (i <= 3);
 
