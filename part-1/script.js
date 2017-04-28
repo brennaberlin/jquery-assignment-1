70b2093593122d184f891c2ea989edfc129421c6
Brenna Berlin <brenna@id.iit.edu>
2017-04-27 23:43:38 -0500

    jquery

--- part-1/script.js
+++ part-1/script.js
@@ -1,1 +1,28 @@
 // Do your work in this file.
+var sandwich = $(".sandwich");
+
+console.log(sandwich);
+
+var ourSandwich = $("#sandwich-cheese");
+
+// console.log(ourSandwich.width());
+// console.log(ourSandwich.height());
+// console.log(sandwich.length);
+// console.log(sandwich.togleClass("cheese"));
+
+// ourSandwich.addClass('food delicious');
+// ourSandwich.removeClass('delicious');
+
+var lettuce = $('.lettuce');
+
+function lettuceClicked(){
+    var newHeight = $(this).height();
+    // console.log(height);
+   newHeight = newHeight + 20;
+
+    $(this).height(newHeight);
+}
+
+lettuce.click(lettuceClicked);
+
+
